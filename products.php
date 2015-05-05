08fddb039283eb86f46eda41400bffe5378639be
Heath Sias <heathsias@gmail.com>
2015-05-04 21:55:26 -0700

    squash javascript bugs

--- products.php
+++ products.php
@@ -8,7 +8,6 @@
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<meta http-equiv="refresh" content="60" />
 		<link rel="stylesheet" type="text/css" href="css/global.css" />
-		<script type="text/javascript" src="scripts/products.js"></script>
 	</head>
 
 	<body>
@@ -25,65 +24,71 @@
 
 				<div class="ProductList">
 					<span class="ProductHeading">Accessories</span>
-					<form id="mogTee" action="" onsubmit="addProduct('mogTee')">
-							<fieldset>
+					<form id="productForm" action="" onsubmit="processProductForm()">
+						<fieldset>
+							<h3 id="teeDescription">Merchant of Games (M.O.G.) T-Shirt</h3>
+							<p>
+								These t-shirts are made from the softest elf-hide in the lower 48.
+								Not only are they guaranteed not to shrink, they also grant
+								the wearer night vision once per day for d4 x 60 secs. Amazing.
+							</p>
+							M.O.G. Tee &nbsp;
+							$<span id="teePrice">10.00</span>
+							&nbsp;&nbsp;
+							<select name="teeStyle">
+								<option>X-Small</option>
+								<option>Small</option>
+								<option>Medium</option>
+								<option>X-Medium</option>
+								<option>Large</option>
+								<option>X-Large</option>
+							</select>&nbsp;&nbsp;
+							Quantity:
+							<input type="text" name="teeQuantity" size="2" />
+							<div><hr /></div>
 
-								<h3>Merchant of Games T-Shirt - $9.99</h3>
-								<p>
-									These t-shirts are made from the softest elf-hide in the lower 48.
-									Not only are they guaranteed not to shrink, they also grant
-									the wearer night vision once per day for d4 x 60 secs. Amazing.
-								</p>
-								M.O.G. Tee&nbsp;&nbsp;
-								<select name="size">
-									<option>X-Small</option>
-									<option>Small</option>
-									<option>Medium</option>
-									<option>X-Medium</option>
-									<option>Large</option>
-									<option>X-Large</option>
-								</select>&nbsp;&nbsp;
-								Quantity:
-								<input type="text" name="quantity" size="2" />
-								<hr />
+							<h3 id="mugDescription">Merchant of Games (M.O.G.) Travel Mug</h3>
+							<p>
+								This mug will never spill its contents, so long as you keep it
+								perfectly upright and/or don't put anything in it. Astounding.
+							</p>
+							M.O.G. Mug &nbsp;
+							$<span id="mugPrice">7.50</span>
+							&nbsp;&nbsp;
+							<select name="mugStyle">
+								<option>12 oz.</option>
+								<option>22 oz.</option>
+								<option>40 oz.</option>
+							</select>&nbsp;&nbsp;
+							Quantity:
+							<input type="text" name="mugQuantity" size="2" />
+							<div><hr /></div>
 
-								<h3>Merchant of Games Travel Mug - $7.99</h3>
-								<p>
-									This mug will never spill its contents, so long as you keep it
-									perfectly upright and/or don't put anything in it. Astounding.
-								</p>
-								M.O.G. Mug&nbsp;&nbsp;
-								<select name="size">
-									<option>12 oz.</option>
-									<option>22 oz.</option>
-									<option>40 oz.</option>
-								</select>&nbsp;&nbsp;
-								Quantity:
-								<input type="text" name="quantity" size="2" />
-								<hr />
+							<h3 id="diceDescription">Merchant of Games (M.O.G.) Dice</h3>
+							<p>
+								These dice are so chock-full of numbers, there's not even any
+								room for the Merchant of Games logo on them. Wait, our logo IS
+								a die. Mind blown.
+							</p>
+							M.O.G. Dice &nbsp;
+							$<span id="dicePrice">5.00</span>
+							&nbsp;&nbsp;
+							<select name="diceStyle">
+								<option>Silver</option>
+								<option>Gold</option>
+								<option>Space Gray</option>
+							</select>&nbsp;&nbsp;
+							Quantity:
+							<input type="text" name="diceQuantity" size="2" />
+							<div><hr /><br /></div>
 
-								<h3>Merchant of Games Dice - $4.99</h3>
-								<p>
-									These dice are so chock-full of numbers, there's not even any
-									room for the Merchant of Games logo on them. Wait, our logo IS
-									a die. Mind blown.
-								</p>
-								M.O.G. Dice&nbsp;&nbsp;
-								<select name="size">
-									<option>Silver</option>
-									<option>Gold</option>
-									<option>Space Grey</option>
-								</select>&nbsp;&nbsp;
-								Quantity:
-								<input type="text" name="quantity" size="2" />
-								<hr /><br />
-
-								<input id="purchase" type="submit" name="purchase" value="Buy Now" />
-							</fieldset>
-							<br />
-							<fieldset>
-								<h2 class="UnderConstruction">Coming Soon ... CCGs, Tabletop Games, and RPGs, oh my!<h2>
-							</fieldset>
+							<input id="purchase" type="submit" value="Buy Now" />
+							<input id="purchase" type="reset" value="Reset" />
+						</fieldset>
+						<div><br /></div>
+						<fieldset>
+							<h2 class="UnderConstruction">Coming Soon ... CCGs, Tabletop Games, and RPGs, oh my!</h2>
+						</fieldset>
 					</form>
 				</div>
 
@@ -92,5 +97,6 @@
 			include("common/footer.html");
 			?>
 		</div>
+		<script type="text/javascript" src="scripts/products.js"></script>
 	</body>
 </html>
--- scripts/products.js
+++ scripts/products.js
@@ -1,22 +1,47 @@
-function addProduct(product)
+function processProductForm()
 {
-  var productFormObj = document.getElementById(product);
-  var size = productFormObj.size.options[productFormObj.size.selectedIndex].text;
-  var quantity = productFormObj.quantity.value;
-  var everythingOK = true;
+  var productFormObj = document.getElementById('productForm');
 
-  if (!validateQuantity(quantity))
+  // get T-Shirt data
+  var teeDescription = document.getElementById('teeDescription').innerHTML;
+  var teeStyle = productFormObj.teeStyle.options[productFormObj.teeStyle.selectedIndex].text;
+  var teePrice = document.getElementById("teePrice").innerHTML;
+  var teeQuantity = productFormObj.teeQuantity.value;
+
+  // get Travel mug data
+  var mugDescription = document.getElementById("mugDescription").innerHTML;
+  var mugStyle = productFormObj.mugStyle.options[productFormObj.mugStyle.selectedIndex].text;
+  var mugPrice = document.getElementById("mugPrice").innerHTML;
+  var mugQuantity = productFormObj.mugQuantity.value;
+
+  // get Dice data
+  var diceDescription = document.getElementById("diceDescription").innerHTML;
+  var diceStyle = productFormObj.diceStyle.options[productFormObj.diceStyle.selectedIndex].text;
+  var dicePrice = document.getElementById("dicePrice").innerHTML;
+  var diceQuantity = productFormObj.diceQuantity.value;
+
+  var descriptions = [teeDescription, mugDescription, diceDescription];
+  var styles = [teeStyle, mugStyle, diceStyle];
+  var prices = [teePrice, mugPrice, dicePrice];
+  var quantities = [teeQuantity, mugQuantity, diceQuantity];
+
+  // validate quantities
+  var everythingOK = true;
+  if (!validateQuantities(quantities))
   {
     alert("Error: Invalid quantity.");
     everythingOK = false;
   }
 
+  // everything checks out. Display info as alert:
+  // for each item: quantity, style, description, total item cost
+  // total cost
   if (everythingOK)
   {
     /* display for each item: summary, cost, total cost
        then display: total cost
     */
-    
+
     alert("Added to cart!");
     return true;
   }
@@ -26,9 +51,14 @@ function addProduct(product)
 
 
 
-function validateQuantity(quantity)
+function validateQuantities(quantities)
 {
-  if (quantity.length > 0)
-    return !isNaN(quantity);
-  return false;
+  for (var i = 0; i < quantities.length; ++i)
+  {
+    if (isNaN(quantities[i]))
+    {
+      return false;
+    }
+  }
+  return true;
 }
